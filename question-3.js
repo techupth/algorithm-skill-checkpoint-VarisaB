fac2a1b68e8c8fce9f986c93b9ac6c1b28ccf1bf
Namfon <varisa.vb@hotmail.com>
2024-07-30 09:39:09 +0700

    feat: question-3

--- question-3.js
+++ question-3.js
@@ -1,20 +1,20 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
 Answer:
-
+    O(n) เพราะ worst case จะมีการ loop/ทำซ้ำ เท่ากันกับจำนวน input ที่เข้ามา
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
 Answer:
-
+    O(log n) เพราะ worst case จะมีการ loop/ทำซ้ำ น้อยกว่าจำนวน input ที่เข้ามา ซึ่งจำนวนรอบที่ทำซ้ำจะเท่ากับ log(จำนวน input)
 
 */
 
 /* 
 Which function is more efficient and why?
 Answer:
-
+    function findProductPrice is more efficient than function findStudentById เพราะ มีจำนวนรอบของการทำซ้ำน้อยกว่า ใช้เวลาน้อยกว่า
 
 */
