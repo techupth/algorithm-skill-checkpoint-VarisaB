e669b28b7387686f0c27d020fbafd3819c3265b6
Namfon <varisa.vb@hotmail.com>
2024-07-30 09:29:32 +0700

    feat: question-5

--- question-5.js
+++ question-5.js
@@ -1,6 +1,6 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
 Answer:
-
+    O(n^2) เพราะ มีการ loop ซ้อนกัน 2 ชั้น ทำให้จำนวนรอบของการทำซ้ำ มากขึ้นตามจำนวน input ยกกำลัง 2
 
 */
